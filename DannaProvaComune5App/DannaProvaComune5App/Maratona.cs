52393a05a7b1942f4907f7aec7bd9028aa108876
AlessandroDanna <alessandro.danna@icloud.com>
2022-02-16 14:08:09 +0100

    Scritto il codice del leggi da file

---
+++ DannaProvaComune5App/DannaProvaComune5App/Maratona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DannaProvaComune5App
+{
+    class Maratona
+    {
+        //NomeAtleta % SocietaAppartenenza % Ore % Minuti % Citta
+        public string NomeAtleta { get; set; }
+        public string SocietaAppartenenza { get; set; }
+        public string Ore { get; set; }
+        public string Minuti { get; set; }
+        public string Citta { get; set; }
+    }
+}
---
+++ DannaProvaComune5App/DannaProvaComune5App/Maratone.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace DannaProvaComune5App
+{
+    class Maratone
+    {
+        public List<Maratona> elencoMaratone;
+
+        public Maratone()
+        {
+            elencoMaratone = new List<Maratona>();
+        }
+
+        public void Aggiungi(Maratona maratona)
+        {
+            elencoMaratone.Add(maratona);
+        }
+
+        public void LeggiDati()
+        {
+            using (FileStream flussoDati = new FileStream("maratona.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader lettoreDati = new StreamReader(flussoDati))
+                {
+                    while (!lettoreDati.EndOfStream)
+                    {
+                        string linea = lettoreDati.ReadLine();
+                        string[] campi = linea.Split('%');
+
+                        Maratona maratona = new Maratona();
+                        maratona.NomeAtleta = campi[0];
+                        maratona.SocietaAppartenenza = campi[1];
+                        maratona.Ore = campi[2];
+                        maratona.Minuti = campi[3];
+                        maratona.Citta = campi[4];
+
+                        Aggiungi(maratona);
+
+                    }
+                }
+            }
+        }
+
+        /*public string CercaArtista(string titolo)
+        {
+            string artista = titolo + "--NON TROVATO--";
+
+            foreach (var brano in elencoBrani)
+            {
+                if (brano.TitoloBrano == titolo)
+                {
+                    artista = brano.NomeArtista;
+                }
+            }
+
+            return artista;
+        }
+
+        public string ContaBrani(string album)
+        {
+            string numBraniStr = album + "--NON TROVATO--";
+            int numBrani = 0;
+
+            foreach (var brano in elencoBrani)
+            {
+                if (brano.TitoloAlbum == album)
+                {
+                    numBrani++;
+                }
+            }
+
+            if (numBrani != 0)
+            {
+                numBraniStr = numBrani.ToString();
+            }
+            return numBraniStr;
+        }
+
+        public string CalcolaDurata()
+        {
+            string durata = "0";
+
+            return durata.ToString();
+        }*/
+    }
+}
